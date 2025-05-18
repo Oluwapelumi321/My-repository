f2e5631d60f7327bb90c1f075ed5770fd7dcdcdb
Oluwapelumi321 <emmanueloriade70@gmail.com>
2025-05-18 14:01:24 +0100

    Use Render persistent disk for SQLite

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const Database = require('better-sqlite3');
-const db = new Database('users.db');
+const db = new Database('/data/users.db');
 
 // Create users table if it doesn't exist
 db.prepare(`
